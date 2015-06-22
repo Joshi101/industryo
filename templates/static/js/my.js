957fa2c18a48bce5ff5353609400335282ad5896
ARvind Tiwari <tiwariav@gmail.com>
2015-06-22 07:30:20 +0530

    simply

--- templates/static/js/my.js
+++ templates/static/js/my.js
@@ -1,128 +1,45 @@
-// bootstrap-wysiwyg
+/*!
+ * Javascript functions for CoreLogs.com
+ *
+ * Date: 22-06-2015
+ */
 
-$(function(){
-function initToolbarBootstrapBindings() {
-  var fonts = ['Serif', 'Sans', 'Arial', 'Arial Black', 'Courier', 
-        'Courier New', 'Comic Sans MS', 'Helvetica', 'Impact', 'Lucida Grande', 'Lucida Sans', 'Tahoma', 'Times',
-        'Times New Roman', 'Verdana'],
-        fontTarget = $('[title=Font]').siblings('.dropdown-menu');
-  $.each(fonts, function (idx, fontName) {
-      fontTarget.append($('<li><a data-edit="fontName ' + fontName +'" style="font-family:\''+ fontName +'\'">'+fontName + '</a></li>'));
-  });
-  $('a[title]').tooltip({container:'body'});
-    $('.dropdown-menu input').click(function() {return false;})
-        .change(function () {$(this).parent('.dropdown-menu').siblings('.dropdown-toggle').dropdown('toggle');})
-    .keydown('esc', function () {this.value='';$(this).change();});
 
-  $('[data-role=magic-overlay]').each(function () { 
-    var overlay = $(this), target = $(overlay.data('target')); 
-    overlay.css('opacity', 0).css('position', 'absolute').offset(target.offset()).width(target.outerWidth()).height(target.outerHeight());
-  });
-  if ("onwebkitspeechchange"  in document.createElement("input")) {
-    var editorOffset = $('#editor').offset();
-    $('#voiceBtn').css('position','absolute').offset({top: editorOffset.top, left: editorOffset.left+$('#editor').innerWidth()-35});
-  } else {
-    $('#voiceBtn').hide();
-  }
-};
-function showErrorAlert (reason, detail) {
-    var msg='';
-    if (reason==='unsupported-file-type') { msg = "Unsupported format " +detail; }
-    else {
-        console.log("error uploading file", reason, detail);
+/* function to auto adjust top margin for the body */
+    function body_slide () {
+      var top = $('.navbar-fixed-top').outerHeight(true);
+      $('.body').stop().animate({'top':top});  
     }
-    $('<div class="alert"> <button type="button" class="close" data-dismiss="alert">&times;</button>'+ 
-     '<strong>File upload error</strong> '+msg+' </div>').prependTo('#alerts');
-};
-initToolbarBootstrapBindings();  
-$('#editor').wysiwyg({ fileUploadError: showErrorAlert} );
-window.prettyPrint && prettyPrint();
-});
 
-// This function gets cookie with a given name
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
+/* call body_slide when the window loads or resizes */
+    $(body_slide);
+    $(window).on('resize', body_slide);
+
+
+
+/* function to convert rendered form inputs that require tagging (wont be needed soon) */
+    function convert_to_taggable(){
+        var $this = $(this);
+        var name = $this.attr('name')
+        ,   result = $this.data('results');
+        $this.attr('name','');
+        $this.removeClass('taggable').addClass('d_input');
+        if (result == 'multiple')
+            var d_results = '"multiple"><div class="d_results"></div>';
+        else if(result == 'single')
+            var d_results = '"single">';
+        var old_input = $this.clone().wrap('<p>')
+            .parent().html();
+        var hidden_input = '<input type="hidden" name="' + name + '" value="">';
+        var new_input = '<div class="d_search" data-results='+ d_results + old_input + hidden_input + '<div class="dropdown"><ul class="dropdown-menu d_list"></ul></div></div>';
+        $this.replaceWith(new_input);
     }
-    return cookieValue;
-}
-var csrftoken = getCookie('csrftoken');
- 
-/*
-The functions below will create a header with csrftoken
-*/ 
-function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-}
-function sameOrigin(url) {
-    // test that a given url is a same-origin URL
-    // url could be relative or scheme relative or absolute
-    var host = document.location.host; // host + port
-    var protocol = document.location.protocol;
-    var sr_origin = '//' + host;
-    var origin = protocol + sr_origin;
-    // Allow absolute or scheme relative URLs to same origin
-    return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
-        (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
-        // or any other URL that isn't scheme relative or absolute i.e relative.
-        !(/^(\/\/|http:|https:).*/.test(url));
-}
 
-$.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-        if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
-            // Send the token to same-origin, relative URLs only.
-            // Send the token only if the method warrants CSRF protection
-            // Using the CSRFToken value acquired earlier
-            xhr.setRequestHeader("X-CSRFToken", csrftoken);
-        }
-    }
-});
+/* convert each form with class 'taggable' */
+    $('form .taggable').each(convert_to_taggable);
 
 
-// custom written
-
-// function to auto adjust top margin for the body
-
-function body_slide () {
-  var top = $('.navbar-fixed-top').outerHeight(true);
-  $('.body').stop().animate({'top':top});  
-}
-
-$(body_slide);
-$(window).on('resize', body_slide);
-
-// convert rendered form inputs that require tagging
-function convert_to_taggable(){
-    var $this = $(this);
-    var name = $this.attr('name')
-    ,   result = $this.data('results');
-    $this.attr('name','');
-    $this.removeClass('taggable').addClass('d_input');
-    if (result == 'multiple')
-        var d_results = '"multiple"><div class="d_results"></div>';
-    else if(result == 'single')
-        var d_results = '"single">';
-    var old_input = $this.clone().wrap('<p>')
-        .parent().html();
-    var hidden_input = '<input type="hidden" name="' + name + '" value="">';
-    var new_input = '<div class="d_search" data-results='+ d_results + old_input + hidden_input + '<div class="dropdown"><ul class="dropdown-menu d_list"></ul></div></div>';
-    $this.replaceWith(new_input);
-}
-
-$('form .taggable').each(convert_to_taggable);
-
-// dynamic select function
+/* dynamic select function */
 $('.d_input').keyup(function(event){
     var $this = $(this);
     if(event.key == ','){
