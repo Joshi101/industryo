1e7045c4d2091d7f8b6ff66d3df8866ec8214706
sprksh <sprksh.j@gmail.com>
2016-03-15 13:24:31 +0530

    ddd

--- home/email.py
+++ home/email.py
@@ -37,49 +37,48 @@ def send_mail(request):
         subject = request.POST.get('subject')
         arguments = request.POST.get('arguments')
         body = request.POST.get('mail')
-        print(s, body, subject, arguments)
+        print(subject, body, arguments)
         if s == "meh":
             u = User.objects.get(id=1)
-            tasks.send_html_mail(u.id, n=body, subject=subject)
+            tasks.send_html_mail_post(u.id, n=body, subject=subject, arguments=arguments)
         elif s == 'set_wp_all':
             todaydate = date.today()
             startdate = todaydate + timedelta(days=1)
-            enddate = startdate - timedelta(days=100)
+            enddate = startdate - timedelta(days=40)
             lis = User.objects.filter(date_joined__range=[enddate, startdate], userprofile__primary_workplace=None)
             for i in lis:
-                tasks.send_html_mail(i.id, n=body, subject=subject)
+                tasks.send_html_mail_post(i.id, n=body, subject=subject, arguments=arguments)
         # elif s == 'set_wp':
         #     id = request.GET.get('id')
         #     tasks.send_html_mail(id, n=52)
         elif s == "alh":
             users = User.objects.all()
             for u in users:
-                tasks.send_html_mail(u.id, n=body, subject=subject)
+                tasks.send_html_mail_post(u.id, n=body, subject=subject, arguments=arguments)
         elif s == "ah":
             users = User.objects.filter(userprofile__primary_workplace__workplace_type='A')
             for u in users:
-                tasks.send_html_mail(u.id, n=body, subject=subject)
+                tasks.send_html_mail_post(u.id, n=body, subject=subject, arguments=arguments)
         elif s == "bh":
             users = User.objects.filter(userprofile__primary_workplace__workplace_type='B')
             for u in users:
-                tasks.send_html_mail(u.id, n=body, subject=subject)
+                tasks.send_html_mail_post(u.id, n=body, subject=subject, arguments=arguments)
         elif s == "ch":
             users = User.objects.filter(userprofile__primary_workplace__workplace_type='C')
             for u in users:
-                tasks.send_html_mail(u.id, n=body, subject=subject)
+                tasks.send_html_mail_post(u.id, n=body, subject=subject, arguments=arguments)
         elif s == "oh":
             users = User.objects.filter(userprofile__primary_workplace__workplace_type='O')
             for u in users:
-                tasks.send_html_mail(u.id, n=body, subject=subject)
+                tasks.send_html_mail_post(u.id, n=body, subject=subject, arguments=arguments)
         elif s == "nh":
             users = User.objects.filter(userprofile__primary_workplace=None)
             for u in users:
-                tasks.send_html_mail(u.id, n=body, subject=subject)
+                tasks.send_html_mail_post(u.id, n=body, subject=subject, arguments=arguments)
         elif s == "ranh":
             questions = Question.objects.filter(answered=False)
             for q in questions:
-                tasks.send_html_mail(q.user.id, n=Template_answer_your_own_question.format(q.user.userprofile, q.slug, q.title))
-
+                tasks.send_html_mail_post(q.user.id, n=Template_answer_your_own_question.format(q.user.userprofile, q.slug, q.title))
 
         elif s == "lh":
             # pi = []
