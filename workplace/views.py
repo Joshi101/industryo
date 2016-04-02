e8892f09d5dd36b57403a07106d1cf7fbce1e8a2
sprksh <sprksh.j@gmail.com>
2016-04-03 01:54:47 +0530

    dsfsd

--- workplace/views.py
+++ workplace/views.py
@@ -222,29 +222,56 @@ def workplace_dash(request, slug):
     members = UserProfile.objects.filter(primary_workplace=workplace.pk)
     member_count = members.count()
     workplace_logo_form = SetLogoForm()
-    questions = Question.objects.filter(user__userprofile__primary_workplace=workplace).select_related('user')
-    answers = Question.objects.filter(answer__user__userprofile__primary_workplace=workplace).select_related('user')
-    feeds = Node.feed.filter(user__userprofile__primary_workplace=workplace).select_related('user')[:10]
-    articles = Node.objects.filter(user__userprofile__primary_workplace=workplace, category='A').select_related('user')
-    # return render(request, 'workplace/snip_dashboard.html', locals())
-    all_result_list = sorted(
-        chain(feeds, questions, answers, articles),
-        key=attrgetter('date'), reverse=True)
-    paginator = Paginator(all_result_list, 5)
-    page = request.GET.get('page')
-    try:
-        result_list = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        result_list = paginator.page(1)
-    except EmptyPage:
-                # If page is out of range (e.g. 9999), deliver last page of results.
-        return
-                # result_list = paginator.page(paginator.num_pages)
-    if page:
-        return render(request, 'nodes/five_nodes.html', {'result_list': result_list})
+
+    if member_count < 2:
+        n=1
+    elif member_count in range(2,5):
+        n = 2
+    elif member_count in range(5,10):
+        n=3
+    elif member_count in range(10, 20):
+        n=4
     else:
-        return render(request, 'workplace/snip_dashboard.html', locals())
+        n=5
+
+    li = [workplace.mobile_contact1, workplace.mobile_contact1, workplace.website, workplace.fb_page,
+          workplace.linkedin_page, workplace.address, workplace.office_mail_id]
+    
+    print(a)
+
+
+
+    return render(request, 'workplace/snip_dashboard.html', locals())
+
+
+# def activity(request, slug):                              In Place of dashboard
+#     workplace = Workplace.objects.get(slug=slug)
+#     members = UserProfile.objects.filter(primary_workplace=workplace.pk)
+#     member_count = members.count()
+#     workplace_logo_form = SetLogoForm()
+#     questions = Question.objects.filter(user__userprofile__primary_workplace=workplace).select_related('user')
+#     answers = Question.objects.filter(answer__user__userprofile__primary_workplace=workplace).select_related('user')
+#     feeds = Node.feed.filter(user__userprofile__primary_workplace=workplace).select_related('user')[:10]
+#     articles = Node.objects.filter(user__userprofile__primary_workplace=workplace, category='A').select_related('user')
+#     # return render(request, 'workplace/snip_dashboard.html', locals())
+#     all_result_list = sorted(
+#         chain(feeds, questions, answers, articles),
+#         key=attrgetter('date'), reverse=True)
+#     paginator = Paginator(all_result_list, 5)
+#     page = request.GET.get('page')
+#     try:
+#         result_list = paginator.page(page)
+#     except PageNotAnInteger:
+#         # If page is not an integer, deliver first page.
+#         result_list = paginator.page(1)
+#     except EmptyPage:
+#                 # If page is out of range (e.g. 9999), deliver last page of results.
+#         return
+#                 # result_list = paginator.page(paginator.num_pages)
+#     if page:
+#         return render(request, 'nodes/five_nodes.html', {'result_list': result_list})
+#     else:
+#         return render(request, 'workplace/snip_dashboard.html', locals())
 
 
 def workplace_capabilities(request, slug):
@@ -462,7 +489,6 @@ def set_product_details(request):
 @login_required
 def delete_tag(request):
     if request.method == 'POST':
-        print('tag to be deleted')
         user = request.user
         up = user.userprofile
         wp = user.userprofile.primary_workplace
