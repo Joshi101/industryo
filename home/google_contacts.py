8abdebb2f3c52131d4ebb8d4f3e145ecf360b8a1
sprksh <sprksh.j@gmail.com>
2016-04-21 10:15:53 +0530

    sasa

--- home/google_contacts.py
+++ home/google_contacts.py
@@ -40,7 +40,7 @@ import gdata.docs.service
 # Create a client class which will make HTTP requests with Google Docs server.
 client = gdata.docs.service.DocsService()
 # Authenticate using your Google Docs email address and password.
-client.ClientLogin('sprksh.j@gmail.com'.encode(), 'SP.123@tcs'.encode())
+client.ClientLogin('sprksh.j@gmail.com', 'SP.123@tcs')
 
 # Query the server for an Atom feed containing a list of your documents.
 documents_feed = client.GetDocumentListFeed()
@@ -53,7 +53,60 @@ for document_entry in documents_feed.entry:
 
 
 
+from django.contrib.auth.models import User
+# from django.http import get_host
+from django.shortcuts import render_to_response as render
+from django.utils.html import escape
+import gdata.contacts.service
 
+GOOGLE_CONTACTS_URI = 'http://www.google.com/m8/feeds/'
+
+
+
+def get_url_host(request):
+    if request.is_secure():
+        protocol = 'https'
+    else:
+        protocol = 'http'
+    host = request.build_absolute_uri('/')[:-1]
+    return '%s://%s' % (protocol, host)
+
+def get_full_url(request):
+    return get_url_host(request) + request.get_full_path()
+
+def get_auth_sub_url(next):
+    scope = GOOGLE_CONTACTS_URI
+    secure = False
+    session = True
+    contacts_service = gdata.contacts.service.ContactsService()
+    return contacts_service.GenerateAuthSubURL(next, scope, secure, session);
+
+def get_contact_emails(authsub_token):
+    contacts_service = gdata.contacts.service.ContactsService()
+    contacts_service.auth_token = authsub_token
+    contacts_service.UpgradeToSessionToken()
+    emails = []
+    feed = contacts_service.GetContactsFeed()
+    emails.extend(sum([[email.address for email in entry.email] for entry in feed.entry], []))
+    next_link = feed.GetNextLink()
+    while next_link:
+        feed = contacts_service.GetContactsFeed(uri=next_link.href)
+        emails.extend(sum([[email.address for email in entry.email] for entry in feed.entry], []))
+        next_link = feed.GetNextLink()
+    return emails
+
+def import_contacts(request):
+    if request.GET.get('token', ''):
+        emails = get_contact_emails(request.GET['token'])
+        users = User.objects.filter(email__in=emails)
+        return render('google_contacts/results.html', {
+            'users': users
+        })
+    else:
+        next = get_full_url(request)
+        return render('google_contacts/login.html', {
+        'auth_sub_url': get_auth_sub_url(next)
+        })
 
 
 
