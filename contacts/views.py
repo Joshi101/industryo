919e90e4768065abd41a3b13b52f89a60abc1617
sprksh <sprksh.j@gmail.com>
2016-04-25 09:14:33 +0530

    asasa

--- contacts/views.py
+++ contacts/views.py
@@ -18,7 +18,7 @@ def get_email_google(request):
     b = SocialToken.objects.get(account=a)
     # access = b.token
     access_token = b.token
-    url = 'https://www.google.com/m8/feeds/contacts/default/full' + '?access_token=' + access_token + '&max-results=100'
+    url = 'https://www.google.com/m8/feeds/contacts/default/full' + '?access_token=' + access_token + '&max-results=100' +'&alt=json'
     req = urllib2.Request(url, headers={'User-Agent' : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/534.30 (KHTML, like Gecko) Ubuntu/11.04 Chromium/12.0.742.112 Chrome/12.0.742.112 Safari/534.30"})
     contacts = urllib2.urlopen(req).read()
     contacts_xml = etree.fromstring(contacts)
@@ -31,6 +31,9 @@ def get_email_google(request):
         for address in entry.findall('{http://schemas.google.com/g/2005}email'):
             email = address.attrib.get('address')
             result.append(email)
+        for address in entry.findall('{http://schemas.google.com/g/2005}fullName'):
+            fullName = address.attrib.get('fullName')
+            result.append(fullName)
     return render(request, 'search/random_text_print.html', locals())
 
 
