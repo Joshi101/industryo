af68029d603b31c41c376cecd618e444be67f332
sprksh <sprksh.j@gmail.com>
2016-07-28 20:07:51 +0530

    dsada

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from products.models import Products
+from products.models import Products, Category
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['product', 'producer', 'user', 'target_segment']
 
 admin.site.register(Products, ProductAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'level', 'slug']
+
+admin.site.register(Category, CategoryAdmin)
 # Register your models here.
