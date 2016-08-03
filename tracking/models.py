a594bddbd346d6a1720121dc3eac6472530fda9c
sprksh <sprksh.j@gmail.com>
2016-08-03 11:18:48 +0530

    fdsf

--- tracking/models.py
+++ tracking/models.py
@@ -6,7 +6,7 @@ class Tracker(models.Model):
     session = models.ForeignKey(Session, null=True, blank=True)
     source_type = (('1', 'direct'), ('2', 'from_email'), ('3', 'from_some other_SME'), ('4', 'product_snippet'),
                    ('5', 'workplace_snippet'), ('6', 'google_contact_invite'), ('7', 'from referral'))
-    source = models.CharField(max_length=2, choices=source_type)
+    source = models.CharField(max_length=2, choices=source_type, null=True, blank=True)
     source_url = models.URLField(null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
 
