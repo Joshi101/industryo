82180946ccdf9452301dd1624a1530231458c4f1
sprksh <sprksh.j@gmail.com>
2016-08-08 10:52:09 +0530

    dsds

--- inbox/views.py
+++ inbox/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def inbox(request):
     user = request.user
-    
+
     return render(request, 'inbox/inbox.html', locals())
 
 # Create your views here.
