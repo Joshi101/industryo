fda52a313949a2e33a861be7bd1662c170341569
sprksh <sprksh.j@gmail.com>
2016-08-10 10:08:52 +0530

    sds

--- inbox/views.py
+++ inbox/views.py
@@ -40,5 +40,36 @@ def inbox(request):
         return render(request, 'inbox/inbox.html', {'result_list': result_list})
 
 
+@login_required
+def inbox_received(request):
+    user = request.user
+    wp = user.userprofile.primary_workplace
+    inquiries = Enquiry.objects.filter(user=user).order_by('date')
+    quotations = Reply.objects.filter(user=user).order_by('date')
+    # messages = Message.objects.filter()
+    conversations = Conversation.objects.filter(Q(user1=user) | Q(user2=user)).order_by('last_active')
+
+    all_result_list = sorted(
+        chain(inquiries, quotations, conversations),
+        key=attrgetter('date'), reverse=True)
+
+    paginator = Paginator(all_result_list, 20)
+
+    page = request.GET.get('page')
+    try:
+        result_list = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        result_list = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        return
+        # result_list = paginator.page(paginator.num_pages)
+    if page:
+        return render(request, 'nodes/five_nodes.html', {'result_list': result_list})
+    else:
+        return render(request, 'inbox/inbox.html', {'result_list': result_list})
+
+
 
 # Create your views here.
